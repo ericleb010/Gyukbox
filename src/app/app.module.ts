c2e56cb36a1128a6d413fd0ae07490f5d2477c7a
Oliver Zsivanov <oliver.zsivanov@indexexchange.com>
2018-06-26 15:30:54 -0400

    Updated to Angular 6. Renamed test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
+import { TestComponent } from './video-player/video-player.component';
 
 
 @NgModule({
--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,34 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'gyukbox-test',
-  template: `
-  <iframe id="player"
-    width="640"
-    height="360"
-    src="https://www.youtube.com/embed/ASf25UGveBQ?controls=0&disablekb=1&modestbranding=1&showinfo=0&iv_load_policy=3&start=0&enablejsapi=1&rel=0"
-    frameborder="0"
-    style="border: solid 4px #37474F"
-  ></iframe>
-  `,
-  styles: [`.max-width-1024 { max-width: 1024px; margin: 0 auto; }`],
+  templateUrl: './video-player.html',
+  styleUrls: ['./video-player.scss'],
 })
 export class TestComponent implements OnInit {
-  public YT: any;
-  public video: any;
-  public player: any;
-  public reframed: Boolean = false;
+  YT: any;
+  video: any;
+  player: any;
+  reframed = false;
+
   constructor() { }
   init() {
-    var tag = document.createElement('script');
+    const tag = document.createElement('script');
     tag.src = 'https://www.youtube.com/iframe_api';
-    var firstScriptTag = document.getElementsByTagName('script')[0];
+    const firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
   }
 
   ngOnInit() {
     this.init();
-    this.video = '1cH2cerUpMQ' //video id
+    this.video = '1cH2cerUpMQ'; // video id
 
     window['onYouTubeIframeAPIReady'] = (e) => {
       this.YT = window['YT'];
@@ -40,56 +34,56 @@ export class TestComponent implements OnInit {
           'onError': this.onPlayerError.bind(this),
         },
         config: {
-            controls: 0,
-            disablekb: 1,
-            modestbranding: 1,
-            showinfo: 0,
-            iv_load_policy: 3
-        }
-      });
-      window['YT'].config = {
           controls: 0,
           disablekb: 1,
           modestbranding: 1,
           showinfo: 0,
           iv_load_policy: 3
+        }
+      });
+      window['YT'].config = {
+        controls: 0,
+        disablekb: 1,
+        modestbranding: 1,
+        showinfo: 0,
+        iv_load_policy: 3
       };
     };
   }
 
   onPlayerStateChange(event) {
-    console.log(event)
+    console.log(event);
     switch (event.data) {
       case window['YT'].PlayerState.PLAYING:
-        if (this.cleanTime() == 0) {
+        if (this.cleanTime() === 0) {
           console.log('started ' + this.cleanTime());
         } else {
           console.log('playing ' + this.cleanTime())
         };
         break;
       case window['YT'].PlayerState.PAUSED:
-        if (this.player.getDuration() - this.player.getCurrentTime() != 0) {
+        if (this.player.getDuration() - this.player.getCurrentTime() !== 0) {
           console.log('paused' + ' @ ' + this.cleanTime());
         };
         break;
       case window['YT'].PlayerState.ENDED:
         console.log('ended ');
         break;
-    };
-  };
-  //utility
+    }
+  }
+  // utility
   cleanTime() {
-    return Math.round(this.player.getCurrentTime())
-  };
+    return Math.round(this.player.getCurrentTime());
+  }
   onPlayerError(event) {
     switch (event.data) {
       case 2:
-        console.log('' + this.video)
+        console.log('' + this.video);
         break;
       case 100:
         break;
       case 101 || 150:
         break;
-    };
-  };
+    }
+  }
 }
