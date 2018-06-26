aaa6e834f19db202c15b01c8abf3e9aef9d10e89
Peter Bulford <peter.bulford@indexexchange.com>
2018-06-26 19:03:03 -0400

    adding rooms json

--- api/models/rooms.js
+++ api/models/rooms.js
@@ -7,32 +7,42 @@ const Rooms = function Rooms () {
 	let rooms = {};
 
 	this.add = function(name) {
+		let msg = {ROOMS: "Adding a room" + name,	status: "success"};
 		if (typeof name !== 'undefined' && typeof rooms[name] === 'undefined') {
 			rooms[name] = RoomFactory.new(name);
-			log.info({ROOMS: "Adding a room" + name});
+			log.info(msg);
 			return true;
 		}
-		log.info({ROOMS: "Adding a room - FAILED"});
+		msg[status] = "FAILED";
+		log.info(msg);
 		return false;
 	};
 
 	this.get = function(name) {
+		let msg = {ROOMS: "Getting a room" + name,	status: "success"};
 		if (typeof name !== 'undefined' && typeof rooms[name] !== 'undefined') {
-			log.info({ROOMS: "Getting a room " + name});
+			log.info(msg);
 			return rooms[name];
 		}
-		log.info({ROOMS: "Getting a room - FAILED"});
+		msg[status] = "FAILED";
+		log.info(msg);
 		return;
 	}
 
 	this.list = function() {
+		return this;
+	}
+
+	this.json = function() {
 		let roomlist = [];
+		let msg = {ROOMS: "Getting all rooms",	status: "success"};
 		Object.keys(rooms).forEach(key => {
 			roomlist.push(rooms[key].json());
 		});
+		log.info(msg);
 		return roomlist;
-	};
-};
+	}
+}
 
 const self = new Rooms();
 
