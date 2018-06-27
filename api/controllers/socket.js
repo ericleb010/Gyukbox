37e7176936ef8ff56d2f3d400297bf471e19c8e5
Mozes Magyar <mozes.magyar@indexexchange.com>
2018-06-26 23:33:12 -0400

    added base socket

---
+++ api/controllers/socket.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const log = console.log;
+const socketio = require('socket.io');
+const rooms = require('../models/rooms');
+
+module.exports = function (server) {
+
+	const io = socketio(server);
+
+	io.on('connect', function (client) {
+		log.info({'route':'socket','action':'connect','data':data});
+
+		let room;
+
+		client.on('join', function (data) {
+		 	log.info({'route':'socket','action':'join','data':data});
+
+			room = rooms.get(data.room);
+
+			room.addUser(client);
+		});
+
+		client.on('disconnect', function(data) {
+			log.info({'route':'socket','action':'disconnect','data':data});
+
+			room.removeUser(client);
+		});
+	});
+};
